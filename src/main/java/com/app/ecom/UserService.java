4f4d7a94db223e8cb9eeb774d20225d548e112ec
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2025-07-24 18:51:02 +0300

    monolithic ecom app

---
+++ src/main/java/com/app/ecom/UserService.java
@@ -0,0 +1,28 @@
+package com.app.ecom;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private List<User> userList = new ArrayList<>();
+    private Long nextId = 1L;
+
+    public List<User> fetchAllUsers(){
+        return userList;
+    }
+
+    public void addUser(User user){
+        user.setId(nextId++);
+        userList.add(user);
+     }
+
+    public User fetchUser(Long id) {
+     return userList.stream()
+             .filter(user -> user.getId().equals(id))
+             .findAny().get();
+    }
+}
+
